8f719938328594956eed1f58cd8cd9a9c02db1f9
ルーテイジ <@rootage>
2016-04-26 17:13:03 +0900

    まとめ課題コミット

---
+++ src/matome/MatomeExampleMain.java
@@ -0,0 +1,27 @@
+package matome;
+import java.util.List;
+
+
+public class MatomeExampleMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		String str = "あaいうbcえあか12いげgいあわ53347";
+
+		MatomeExample m = new MatomeExample();
+
+		//int型は3以上
+		List<String> l = m.getDivideString(str , 4);
+
+		for(int i = 0; i < l.size(); i++){
+
+			System.out.println(l.get(i));
+
+		}
+
+	}
+
+}
